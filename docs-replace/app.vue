b04bdd15031abd8965febb34fb704149415b7b18
楷鹏:) <wkpcoder@163.com>
2024-01-08 23:35:17 +0800

    Refactor file upload and merge functionality

--- docs-replace/app.vue
+++ docs-replace/app.vue
@@ -1,31 +1,27 @@
 <template>
-  <div>
-    <!-- <el-upload v-model:file="csvFile" class="upload-demo" :on-preview="handlePreview" :on-remove="handleRemove"
-      :before-remove="beforeRemove" :limit="3" :on-exceed="handleExceed">
-      <el-button type="primary">上传 CSV 文件</el-button>
-      <template #tip>
-        <div class="el-upload__tip">
-          上传的文件只能是 csv 格式
-        </div>
-      </template>
+  <div class="container">
+    <el-upload action="#" :auto-upload="false" :on-change="handleDocsUpload" drag>
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">
+        <em>
+          上传 DOCX 模板文件
+        </em>
+        ，填充变量请用花括号形式： {当前公司}
+      </div>
     </el-upload>
-    <el-upload v-model:file="docsFile" class="upload-demo" :on-preview="handlePreview" :on-remove="handleRemove"
-      :before-remove="beforeRemove" :limit="3" :on-exceed="handleExceed">
-      <el-button type="primary">上传 Doc 文件</el-button>
-      <template #tip>
-        <div class="el-upload__tip">
-          上传的文件只能是 Doc 格式，记得 $1$、$2$ 对应的是 CSV 文件的第一列和第二列，以此类推
-        </div>
-      </template>
-    </el-upload> -->
-    <el-upload action="#" :auto-upload="false" :on-change="handleDocsUpload">
-      <el-button slot="trigger" size="small" type="primary">上传 DOCX 模板文件，填充变量请用花括号形式： {company}</el-button>
+    <el-upload action="#" :auto-upload="false" :on-change="handleCSVUpload" drag>
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">
+        <em>
+          上传 CSV 文件
+        </em>
+        ，第一列是填充变量的名字，比如 当前公司、法人
+      </div>
     </el-upload>
-    <el-upload action="#" :auto-upload="false" :on-change="handleCSVUpload">
-      <el-button slot="trigger" size="small" type="primary">上传 CSV 文件，第一列是填充变量的名字，比如 company</el-button>
-    </el-upload>
-
-    <el-button @click="merge">合成</el-button>
+    <el-input v-model="outputName" placeholder="输出每个 .docx 名称，名称为填充变量，比如：当前公司名、法人，不填的话默认为连续数字" />
+    <el-space>
+      <el-button @click="merge">合成</el-button>
+    </el-space>
   </div>
 </template>
 <script lang="tsx" setup>
@@ -36,11 +32,13 @@ import Papa from 'papaparse'
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import PizZipUtils from "pizzip/utils/index.js";
-import { saveAs } from "file-saver";
+import FileSaver from "file-saver";
 import JSZip from 'jszip';
+import { UploadFilled } from '@element-plus/icons-vue'
 
 const csvFile = ref<UploadUserFile>()
 const docsFile = ref<UploadUserFile>()
+const outputName = ref<string>('')
 
 const handleCSVUpload = (file) => {
   console.log(file)
@@ -114,21 +112,26 @@ const merge = async () => {
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     });
 
-    // Output the document using Data-URI
-    // saveAs(out, `output-${index}.docx`);
-
-    files.push(out)
+    files.push({
+      name: `${outputName.value || index}.docx`,
+      file: out
+    })
   }
 
   console.log(files)
 
   const zip = new JSZip();
-  files.map((file, index) => {
-    zip.file(`output-${index}.docx`, file)
-  })
+  files.map(({ name, file }) => zip.file(name, file))
 
   const content = await zip.generateAsync({ type: "blob" });
   console.log(content)
-  saveAs(content, "output.zip");
+  FileSaver.saveAs(content, "output.zip");
 }
 </script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
